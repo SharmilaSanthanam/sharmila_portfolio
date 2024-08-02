619026d3d1ec98c7ff4e59919306def2a836d1c9
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-08-02 21:53:59 +0530

    Update experience.jsx

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -2,7 +2,7 @@ import "./experience.css";
 import Musescore from "../../images/musescore.png"
 import Mern from "../../images/merndev.png"
 import Adobe from "../../images/adobe.png"
-import Content from "../../images/adobe.png"
+import Content from "../../images/Content.jpg"
 import { Col, Container, Row } from "react-bootstrap";
 
 const Experience = () => {
