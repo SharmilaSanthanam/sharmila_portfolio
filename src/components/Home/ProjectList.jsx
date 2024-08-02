ee5536f23eca026189c014731e5f4e6590a25f3e
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-08-02 21:45:35 +0530

    Update ProjectList.jsx

--- src/components/Home/ProjectList.jsx
+++ src/components/Home/ProjectList.jsx
@@ -15,17 +15,7 @@ const ProjectList = () => {
         {projects.map((item) => (
           <Project key={item.id} img={item.img} link={item.link} />
         ))}
-       {* 
-       <div className="pl-texts">
-        <p className="pl-desc">
-        <a href={"https://github.com/SharmilaSanthanam/RentUrBookFrontend"} download style={{color:"green"}}>Frontend source code</a>
-       &nbsp;  &nbsp;
-    
-        <a href={"https://github.com/SharmilaSanthanam/RentUrBookBackend"} download style={{color:"green"}}>Backend source code</a> 
-        
-        </p>
-        </div>
-       *}
+     
       </div>
      
     </div>
