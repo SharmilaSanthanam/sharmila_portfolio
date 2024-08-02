fb25a895443d00a3f76161af2a3536cac0871990
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-08-02 21:47:14 +0530

    Update ProjectList.jsx

--- src/components/Home/ProjectList.jsx
+++ src/components/Home/ProjectList.jsx
@@ -11,13 +11,7 @@ const ProjectList = () => {
           Have a glance at the project by clicking on the below image...
         </p>
       </div>
-      <div className="pl-list">
-        {projects.map((item) => (
-          <Project key={item.id} img={item.img} link={item.link} />
-        ))}
-     
-      </div>
-     
+         
     </div>
   );
 };
