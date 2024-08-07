4abc949be7d0cc7ba57d9b0bdbf12d608a8a2628
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-08-07 22:17:27 +0530

    Update resumepdf.js

--- src/components/Home/resumepdf.js
+++ src/components/Home/resumepdf.js
@@ -4,7 +4,7 @@ import Resume from "../../images/Resume.pdf";
 const ResumePdf = () => {
   return (
     <div> 
-       <a href={"https://drive.google.com/file/d/1XVBY_zfIV8Rs3aF9rEfOiYtL7sRySJGR/view?usp=sharing"} download style={{color:"green"}}>Click here to download RESUME!!!</a>
+       <a href={"https://drive.google.com/file/d/1XVBY_zfIV8Rs3aF9rEfOiYtL7sRySJGR/view"} download style={{color:"green"}}>Click here to download RESUME!!!</a>
          <object width="100%" height="500" data={Resume} type="application/pdf">
         
          </object>
