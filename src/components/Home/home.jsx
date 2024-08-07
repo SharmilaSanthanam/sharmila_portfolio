965cddf13aa302923ae1edc0fa9cebb5dec0b225
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-08-07 22:17:04 +0530

    Update home.jsx

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
             </div>
           </div>
           <div> 
-          <a href={"https://drive.google.com/file/d/1XVBY_zfIV8Rs3aF9rEfOiYtL7sRySJGR/view?usp=sharing"} target="_blank" rel="noreferrer" className="resume" download style={{color:"green"}}>
+          <a href={"https://drive.google.com/file/d/1XVBY_zfIV8Rs3aF9rEfOiYtL7sRySJGR/view"} target="_blank" rel="noreferrer" className="resume" download style={{color:"green"}}>
           Click to view the Resume!!!
       </a>
        </div>
