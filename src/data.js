b2cbf3fecf8e791c663f3efdf9fb806ca2c41cb4
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-11-22 13:45:48 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,12 +4,12 @@ export const projects = [
       id: 1,
       img: "image/videoedit.jpg",
       link: "https://www.youtube.com/@tejvision"
-    }
+    },
      {
       id: 2,
       img: "image/bookcover.png",
       link: "https://a.co/d/fnB1Zkz"
-    }
+    },
  {
       id: 3,
       img: "image/bookcover1.png",
