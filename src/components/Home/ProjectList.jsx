ad728e11f292719438727d0ad4d4f570aeb7aaf6
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-11-22 13:13:20 +0530

    Update ProjectList.jsx

--- src/components/Home/ProjectList.jsx
+++ src/components/Home/ProjectList.jsx
@@ -4,16 +4,29 @@ import { projects } from "../../data";
 
 const ProjectList = () => {
  return (
-    <div className="pl">
+   <div className="pl">
       <div className="pl-texts">
         <h1 className="pl-title">Create & inspire!!!</h1>
         <p className="pl-desc">
           Have a glance at the project by clicking on the below image...
         </p>
       </div>
-         
+      <div className="pl-list">
+        {projects.map((item) => (
+          <Project key={item.id} img={item.img} link={item.link} />
+        ))}
+             <div className="pl-texts">
+        <p className="pl-desc">
+        <a href={"www.youtube.com/@tejvision"} download style={{color:"green"}}>Frontend source code</a>
+       &nbsp;  &nbsp;
+    
+        <a href={"www.youtube.com/@tejvision"} download style={{color:"green"}}>Backend source code</a> 
+        
+        </p>
+        </div>
+      </div>
+     
     </div>
   );
 };
-
 export default ProjectList;
